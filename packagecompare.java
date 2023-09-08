cb586a0596fad428532b30e1a284823bdaed581a
Ashish khadka <khadkaashish50@gmail.com>
2023-09-08 10:10:32 +0545

    latest

--- packagecompare.java
+++ packagecompare.java
@@ -3,6 +3,7 @@ public class packagecompare {
     public static void main(String []args){
         double x=30;
         double y=5;
+        Integer in =12;
         System.out.println("minimum number of xa and y is:"+ Math.max(x, y));
         System.out.println("Square root of  y is:" +Math.sqrt(y));
         System.out.println("logarithms of x is:" +Math.log(x));
@@ -12,6 +13,11 @@ public class packagecompare {
         System.out.println("lod1p of x is:" +Math.log1p(x));
         System.out.println("exp of a is:" +Math.exp(x));
         System.out.println("expm1 of a is:" +Math.expm1(x));
-
+        System.out.println(in.toString());
+        System.out.println(in.toString().getClass());
+        System.out.println(Integer.toString(12));
+        System.out.println(Integer.toBinaryString(152));
+        System.out.println(Integer.toHexString(152));
+        System.out.println(Integer.toOctalString(152)); 
     }
 }
