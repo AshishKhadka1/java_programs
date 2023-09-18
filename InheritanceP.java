3b087671fab49b16a0d74ccfcc39764ef30b2ade
Ashish khadka <khadkaashish50@gmail.com>
2023-09-18 12:59:56 +0545

    latest

---
+++ InheritanceP.java
@@ -0,0 +1,32 @@
+class Animal{
+
+    String name;
+    Animal(String name){
+        this.name = name;
+    }
+ void makeSound(){
+    System.out.println(name +"makes a sound.");
+ }
+}
+class Dog extends Animal{
+    Dog(String name){
+        super(name);
+    }
+void makeSound(){
+    System.out.println(name +"barks.");
+}
+void wagTail() {
+    System.out.println(name + "wags it's tail");
+ }
+}
+
+public class InheritanceP {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal("Generic Animal b n ");
+        myAnimal.makeSound();
+        Dog mydog = new Dog("Buddy");
+        mydog.makeSound();
+        mydog.wagTail();
+    }
+}
+
