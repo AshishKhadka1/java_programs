01dc2769285f7b821489488189d82caad6d8fc67
Ashish khadka <khadkaashish50@gmail.com>
2023-09-18 21:49:10 +0545

    latest

---
+++ myClass.java
@@ -0,0 +1,9 @@
+public class myClass {
+    static void myMethod(){
+        System.out.println("i am god");
+    }
+    public static void main(String[] args){
+        myMethod(); // method Calling
+    }
+}
+
