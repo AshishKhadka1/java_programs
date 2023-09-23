1e1cd48b5720a9683b7353d3046e3acde9c43280
Ashish khadka <khadkaashish50@gmail.com>
2023-09-23 11:24:03 +0545

    latest

---
+++ ExceptionHandlingEg.java
@@ -0,0 +1,11 @@
+public class ExceptionHandlingEg {
+    public static void main(String[] args){
+       int[] marks ={19, 40, 48};
+       try {
+           System.out.println(marks[5]);
+            }catch(Exception exception){
+
+            }
+        System.out.println("aman");
+        }
+    }
